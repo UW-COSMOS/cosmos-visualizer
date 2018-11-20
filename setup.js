174293465b68b7275fec0c933dde141b0d082aee
John J Czaplewski <john@czaplewski.org>
2018-11-20 11:44:19 -0600

    Parameterize setup

--- setup.js
+++ setup.js
@@ -4,6 +4,13 @@ const asyncEachLimit = require('async/eachLimit')
 const sqlite3 = require('sqlite3')
 const db = new sqlite3.Database(`${__dirname}/annotations.sqlite`)
 
+const folder = process.argv[2]
+const datasetName = process.argv[3] || folder
+if (!folder) {
+    console.log('No folder of docs specified. Example: node setup.js docs1 <optional dataset name>')
+  process.exit()
+}
+
 function setup(callback) {
   db.serialize(() => {
     db.get(`SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'images';`, (error, row) => {
@@ -22,9 +29,9 @@ function setup(callback) {
 
 setup(() => {
   // Read the folder
-  asyncEachLimit(fs.readdirSync(`${__dirname}/cosmos_docs`), 1, (entry, callback) => {
+  asyncEachLimit(fs.readdirSync(`${__dirname}/${folder}`), 1, (entry, callback) => {
     // For each entry check if it is a directory
-    let stats = fs.statSync(`${__dirname}/cosmos_docs/${entry}`)
+    let stats = fs.statSync(`${__dirname}/${folder}/${entry}`)
 
     // If it is a directory it is a document
     if (stats.isDirectory()) {
@@ -32,12 +39,12 @@ setup(() => {
       const doc_id = entry
 
       // Get all the pages for this document and insert them into the database
-      asyncEachLimit(fs.readdirSync(`${__dirname}/cosmos_docs/${doc_id}/png`), 1, (page, done) => {
+      asyncEachLimit(fs.readdirSync(`${__dirname}/${folder}/${doc_id}/png`), 1, (page, done) => {
         let parts = page.split('_')
         if (parts.length) {
           const page_no = parts[1]
 
-          db.run(`INSERT INTO images (image_id, doc_id, page_no, stack, file_path) VALUES (?, ?, ?, ?, ?)`, [uuidv4(), doc_id, page_no, 'cosmos1', `cosmos_docs/${doc_id}/png/${page}`], (error) => {
+          db.run(`INSERT INTO images (image_id, doc_id, page_no, stack, file_path) VALUES (?, ?, ?, ?, ?)`, [uuidv4(), doc_id, page_no, datasetName, `${folder}/${doc_id}/png/${page}`], (error) => {
             done()
           })
         } else {
