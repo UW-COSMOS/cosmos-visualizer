52eb71246fb6c5c3e61b68ee1e19f6a6f9a9c0b4
Daven Quinn <dev@davenquinn.com>
2019-02-13 23:08:20 -0600

    Moved coffeescript back to javascript

--- api/setup.js
+++ api/setup.js
@@ -1,65 +1,86 @@
-const fs = require('fs')
+const PGPromise = require('pg-promise')
 const uuidv4 = require('uuid/v4')
-const asyncEachLimit = require('async/eachLimit')
-const sqlite3 = require('sqlite3')
-const db = new sqlite3.Database(`${__dirname}/annotations.sqlite`)
+const fs = require('fs')
+const {join} = require('path')
+const {TSParser} = require('tsparser')
 
 const folder = process.argv[2]
 const datasetName = process.argv[3] || folder
+
 if (!folder) {
-    console.log('No folder of docs specified. Example: node setup.js docs1 <optional dataset name>')
+  console.log('No folder of docs specified. Example: node setup.js docs1 <optional dataset name>')
   process.exit()
 }
 
-function setup(callback) {
-  db.serialize(() => {
-    db.get(`SELECT name FROM sqlite_master WHERE type = 'table' AND name = 'images';`, (error, row) => {
-      if (error || !row) {
-        let setupSQL = fs.readFileSync(`${__dirname}/schema.sql`, 'utf8')
-        let tagDataSQL = fs.readFileSync(`${__dirname}/tag-data.sql`, 'utf8')
+const pgp = PGPromise({promiseLib: Promise, pgNative: true});
+const db = pgp("postgres://postgres@db:5432/annotations")
 
-        db.exec(setupSQL, (error) => {
-          db.exec(tagDataSQL, (error) => {
-            callback(null)
-          })
-        })
-      } else {
-        callback(null)
-      }
-    })
-  })
+async function sleep(ms) {
+  await new Promise(resolve => { setTimeout(resolve, ms)});
+};
+
+async function runQuery() {
+  try { await db.query.apply(this,arguments) }
+  catch(err) { console.error(err.toString()) }
+};
+
+async function runFromFile(fn) {
+  const SQL = fs.readFileSync(`${__dirname}/${fn}`, "utf8");
+  const statements = TSParser.parse(SQL, 'pg', ';');
+  await db.tx(async function(){
+    for (var statement of statements) {
+      await runQuery(statement);
+    }
+  });
+};
+
+const insertImage = `INSERT INTO image
+                    (image_id, doc_id, page_no, stack, file_path)
+                    VALUES ($1, $2, $3, $4, $5)
+                    ON CONFLICT DO NOTHING`
+
+async function setup() {
+  // Entire setup script using helpers above
+  let connected = false
+  while (!connected) {
+    try {
+      await db.connect();
+      connected = true
+    } catch(err) {
+      console.error("Can't connect to database yet");
+      await sleep(1000);
+    }
+  }
+
+  await runFromFile("schema.sql");
+  await runFromFile("tag-data.sql");
+
+  // Read the folder
+  for (var entry of fs.readdirSync(join(folder))) {
+    let stats = fs.statSync(join(folder, entry))
+    // For each entry check if it is a directory
+    if (!stats.isDirectory()) continue
+    // Now we know this is a document
+    let doc_id = entry
+    // Get all the pages for this document and insert them into the database
+    for (var page of fs.readdirSync(join(folder, doc_id, 'png'))) {
+      let parts = page.split('_')
+      if (!parts.length) continue
+      let page_no = parts[1]
+      let file_path = join(folder, doc_id, 'png', page);
+      await db.query(insertImage, [
+        uuidv4(),
+        doc_id,
+        page_no,
+        datasetName,
+        file_path
+      ]);
+    }
+  }
 }
 
-setup(() => {
-  // Read the folder
-  asyncEachLimit(fs.readdirSync(`${__dirname}/${folder}`), 1, (entry, callback) => {
-    // For each entry check if it is a directory
-    let stats = fs.statSync(`${__dirname}/${folder}/${entry}`)
+(async function(){
+  try { await setup()}
+  catch (err) { console.error(err.toString()) }
+})();
 
-    // If it is a directory it is a document
-    if (stats.isDirectory()) {
-      // Now we know this is a document
-      const doc_id = entry
-
-      // Get all the pages for this document and insert them into the database
-      asyncEachLimit(fs.readdirSync(`${__dirname}/${folder}/${doc_id}/png`), 1, (page, done) => {
-        let parts = page.split('_')
-        if (parts.length) {
-          const page_no = parts[1]
-
-          db.run(`INSERT INTO images (image_id, doc_id, page_no, stack, file_path) VALUES (?, ?, ?, ?, ?)`, [uuidv4(), doc_id, page_no, datasetName, `${folder}/${doc_id}/png/${page}`], (error) => {
-            done()
-          })
-        } else {
-          done()
-        }
-      }, () => {
-        callback()
-      })
-    } else {
-      callback(null)
-    }
-  }, (error) => {
-    console.log('done')
-  })
-})
