e9932a4438ea345616152510871315d19a073fc7
Daven Quinn <dev@davenquinn.com>
2019-04-18 16:14:46 -0500

    Model info route only returns documents and pages

--- api/v1-routes/model-info.js
+++ api/v1-routes/model-info.js
@@ -1,30 +1,8 @@
 const sql = `
-WITH p AS (
 SELECT
 	count(DISTINCT image_id) pages,
 	count(DISTINCT doc_id) documents
-FROM image
-),
-a AS (
-SELECT
-	target_img_path path
-FROM equations.figures_and_tables
-UNION ALL
-SELECT assoc_img_path path
-FROM equations.figures_and_tables
-),
-b AS (
-SELECT DISTINCT ON (path)
-	path
-FROM a
-)
-SELECT
- count(path) FILTER (WHERE path ~ 'Figure\\d') figures,
- count(path) FILTER (WHERE path ~ 'Table\\d') AS tables,
- count(path) FILTER (WHERE path ~ 'Equation\\d') equations,
- (SELECT pages FROM p),
- (SELECT documents FROM p)
-FROM b`
+FROM image`
 
 async function handler(req, res, next, plugins) {
   const {db} = plugins;
