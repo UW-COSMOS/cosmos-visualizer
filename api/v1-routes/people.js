30c649fa0bef4de6edb13a91da908307e61bf758
dyyfk <879608095@qq.com>
2019-06-22 02:21:51 -0500

    remove the api key

--- api/v1-routes/people.js
+++ api/v1-routes/people.js
@@ -1,13 +1,11 @@
 const uuidv4 = require('uuid/v4')
-const apiKey = require('../api_key')
-const {wrapHandler} = require('../handlers/util')
+const { wrapHandler } = require('../handlers/util')
 
 const handler = async (req, res, next, plugins) => {
-  const {db} = plugins;
-  if (req.method === 'GET') {
-    let where = (req.query.person_id != 'all') ? `WHERE person_id = ?` : ''
-    let params = (req.query.person_id != 'all') ? [ req.query.person_id ] : []
-    let people = await db.any(`
+  const { db } = plugins;
+  let where = (req.query.person_id != 'all') ? `WHERE person_id = ?` : ''
+  let params = (req.query.person_id != 'all') ? [req.query.person_id] : []
+  let people = await db.any(`
       SELECT
         person_id,
         name,
@@ -16,36 +14,14 @@ const handler = async (req, res, next, plugins) => {
         created
       FROM person
       ${where}`, params);
-    res.reply(req, res, next, people);
-  } else {
-    // Validate the input
-    let incoming = req.body;
+  res.reply(req, res, next, people);
 
-    if (!incoming.name) {
-      return res.error(req, res, next, 'A "name" must be provided', 400)
-    }
-    if (!incoming.key || incoming.key != apiKey) {
-      return res.error(req, res, next, 'You do not have permission to create a person', 400)
-    }
-    let person_id = uuidv4()
-
-    try {
-      let params = [person_id, incoming.name, incoming.tagger || true, incoming.validator || false ];
-      db.none(`
-        INSERT INTO person (person_id, name, tagger, validator)
-        VALUES ($1, $2, $3, $4)`, params);
-      res.reply(req, res, next, {'person_id': person_id});
-    } catch(error) {
-      return res.error(req, res, next, 'Something went wrong while adding the person', 500)
-    }
-  }
 }
-
 module.exports = {
   path: '/people/:person_id?',
   displayPath: '/people',
-  methods: ['GET', 'POST'],
-  description: 'Retrieve or add a person',
+  methods: ['GET'],
+  description: 'Retrieve a person',
   parameters: {
     'person_id': {
       'positional': true,
@@ -68,8 +44,8 @@ module.exports = {
       'description': 'An API authentication key required to create users'
     }
   },
-  requiredParameters: [ ],
-  requiresOneOf: [ ],
+  requiredParameters: [],
+  requiresOneOf: [],
   fields: {
     'person_id': {
       'type': 'integer',
