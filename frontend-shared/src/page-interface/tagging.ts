02c29255bc82061cf5dfe6fae414c9345aac3167
Daven Quinn <dev@davenquinn.com>
2020-01-30 17:27:41 -0600

    Removed redundant methods from tagging interface

--- frontend-shared/src/page-interface/tagging.ts
+++ frontend-shared/src/page-interface/tagging.ts
@@ -48,15 +48,7 @@ class TaggingPage extends StatefulComponent {
   static contextType = APIContext;
   constructor(props){
     super(props);
-    this.updateAnnotation = this.updateAnnotation.bind(this);
-    this.addLink = this.addLink.bind(this);
-    this.updateCurrentTag = this.updateCurrentTag.bind(this);
-    this.selectAnnotation = this.selectAnnotation.bind(this);
-    this.appendAnnotation = this.appendAnnotation.bind(this);
-    this.toggleTagLock = this.toggleTagLock.bind(this);
-    this.clearChanges = this.clearChanges.bind(this);
     this.currentStackID = this.currentStackID.bind(this);
-    this.setupTags = this.setupTags.bind(this);
     this.onImageLoaded = this.onImageLoaded.bind(this);
 
     this.state = {
@@ -71,102 +63,6 @@ class TaggingPage extends StatefulComponent {
       lockedTags: new Set()
     };
   }
-
-  updateAnnotation(i){ return updateSpec=> {
-    const spec = {rectStore: {[i]: updateSpec}};
-    if (updateSpec.tag_id != null) {
-      spec.currentTag = updateSpec.tag_id;
-    }
-    return this.updateState(spec);
-  }; }
-
-  addLink(i){ return () => {
-    // Add a link to another annotation
-    const {editingRect, rectStore} = this.state;
-    const {image_tag_id} = rectStore[i];
-    if ((editingRect == null)) {
-      throw "Annotation must be selected to add a link";
-    }
-    if (editingRect === i) {
-      throw "Cannot create self-referential link";
-    }
-    const spec = {
-      rectStore: {[editingRect]: {linked_to: {$set: image_tag_id}}}
-    };
-    return this.updateState(spec);
-  }; }
-
-  deleteAnnotation = (i: number) => {
-    const {rectStore, editingRect} = this.state;
-    const spec = {
-      rectStore: {$splice: [[i,1]]}
-    };
-    if ((editingRect != null) && (i === editingRect)) {
-      spec.editingRect = {$set: null};
-    }
-    // Zero out links to this annotation
-    const {image_tag_id} = rectStore[editingRect];
-    for (i = 0; i < rectStore.length; i++) {
-      const rect = rectStore[i];
-      if (rect.linked_to !== image_tag_id) { continue; }
-      spec.rectStore[i] = {linked_to: {$set: null}};
-    }
-    return this.updateState(spec);
-  };
-
-  updateCurrentTag(tag_id){ return () => {
-    console.log(`Current tag: ${tag_id}`);
-    return this.updateState({currentTag: {$set: tag_id}});
-  }; }
-
-  selectAnnotation(i){ return () => {
-    console.log(`Selecting annotation ${i}`);
-    return this.updateState({editingRect: {$set: i}});
-  }; }
-
-  appendAnnotation(rect){
-    if (rect == null) { return; }
-    const {currentTag, rectStore} = this.state;
-    rect.tag_id = currentTag;
-    // Create UUID on client side to allow
-    // linking
-    rect.image_tag_id = uuidv4();
-    return this.updateState({
-      rectStore: {$push: [rect]},
-      editingRect: {$set: rectStore.length}
-    });
-  }
-
-  toggleTagLock(tagId){ return () => {
-    const {tagStore, currentTag, lockedTags} = this.state;
-
-    if (lockedTags.has(tagId)) {
-      lockedTags.delete(tagId);
-    } else {
-      lockedTags.add(tagId);
-    }
-
-    // Check if locked and then get next unlocked tag
-    let ix = tagStore.findIndex(d => d.tag_id===currentTag);
-    let forward = true;
-    while (lockedTags.has(tagStore[ix].tag_id)) {
-      ix += forward ? 1 : -1;
-      if (ix > (tagStore.length-1)) {
-        forward = false;
-        ix -= 1;
-      }
-      if (ix < 0) {
-        forward = true;
-      }
-    }
-
-    const nextTag = tagStore[ix].tag_id;
-    const spec = {lockedTags: {$set: lockedTags}};
-    if (nextTag !== currentTag) {
-      spec.currentTag = {$set: nextTag};
-    }
-    return this.updateState(spec);
-  }; }
 
   clearChanges() {
     const {initialRectStore} = this.state;
@@ -187,27 +83,14 @@ class TaggingPage extends StatefulComponent {
       currentTag,
       lockedTags
     } = this.state;
-    const hasChanges = isDifferent(initialRectStore, rectStore);
     const {editingEnabled} = this.props;
-
-    const actions: AnnotationActions = (() => {
-      let addLink, appendAnnotation, deleteAnnotation, selectAnnotation, toggleTagLock, updateAnnotation, updateCurrentTag, updateState;
-      return ({deleteAnnotation,
-       updateAnnotation,
-       selectAnnotation,
-       appendAnnotation,
-       updateCurrentTag,
-       toggleTagLock,
-       updateState,
-       addLink} = this);
-    })();
 
     return h(TagsProvider, {tags: tagStore}, [
       h(AnnotationEditorProvider, {
         initialAnnotations: initialRectStore
       }, [
         h(PageFrame, {
-          hasChanges,
+          hasChanges: false,
           subtitleText,
           editingEnabled,
           hasInitialContent: initialRectStore.length != 0,
@@ -217,13 +100,8 @@ class TaggingPage extends StatefulComponent {
           getNextImage: this.getImageToDisplay.bind(this)
         }, [
           image == null ? null : h(ImageContainer, {
-            editingRect,
             editingEnabled,
-            image,
-            imageTags: rectStore,
-            lockedTags,
-            currentTag,
-            actions
+            image
           })
         ])
       ])
@@ -339,8 +217,6 @@ class TaggingPage extends StatefulComponent {
   }
 
   componentDidMount() {
-    this.context.get("/tags/all")
-      .then(this.setupTags);
     return this.getImageToDisplay();
   }
 
