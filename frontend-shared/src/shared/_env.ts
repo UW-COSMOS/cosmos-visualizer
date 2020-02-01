e565dee963f8645763ef0710436836e1290a5982
Daven Quinn <dev@davenquinn.com>
2020-02-01 01:09:27 -0600

    Moved some things around

---
+++ frontend-shared/src/shared/_env.ts
@@ -0,0 +1,37 @@
+function getEnvironmentConfig(opts={}){
+  /**
+  Get configuration from environment...
+  this might eventually need some editing for different
+  environments
+  */
+  let {baseURL, imageBaseURL, publicURL} = opts;
+
+  try {
+    // Attempt to set parameters from environment variables
+    // This will fail if bundled on a different system, presumably,
+    // so we wrap in try/catch.
+    publicURL = process.env.PUBLIC_URL;
+    baseURL = process.env.API_BASE_URL;
+    imageBaseURL = process.env.IMAGE_BASE_URL;
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log(`\
+Environment variables:
+PUBLIC_URL: ${process.env.PUBLIC_URL}
+API_BASE_URL: ${process.env.API_BASE_URL}
+IMAGE_BASE_URL: ${process.env.IMAGE_BASE_URL}\
+`
+  );
+
+  // Set reasonable defaults
+  if (publicURL == null) { publicURL = "/"; }
+  if (baseURL == null) { baseURL = "https://dev.macrostrat.org/image-tagger-api"; }
+  if (imageBaseURL == null) { imageBaseURL = "https://dev.macrostrat.org/image-tagger/img/"; }
+
+  console.log(publicURL, baseURL, imageBaseURL);
+  return {publicURL, baseURL, imageBaseURL}
+}
+
+export {getEnvironmentConfig}
--- frontend-shared/src/visualizer-app/index.ts
+++ frontend-shared/src/visualizer-app/index.ts
@@ -7,6 +7,7 @@ import h from 'react-hyperscript';
 import {App} from './app';
 import {APIProvider} from '../api';
 import {ImageStoreProvider} from './image-container';
+import {getEnvironmentConfig} from '~/shared/_env'
 
 const AppHolder = props => {
   const {baseURL, imageBaseURL, publicURL, ...rest} = props;
@@ -18,39 +19,11 @@ const AppHolder = props => {
 }
 
 const createUI = function(opts){
-  if (opts == null) { opts = {}; }
-  let {baseURL, imageBaseURL, publicURL} = opts;
-
-  try {
-    // Attempt to set parameters from environment variables
-    // This will fail if bundled on a different system, presumably,
-    // so we wrap in try/catch.
-    publicURL = process.env.PUBLIC_URL;
-    baseURL = process.env.API_BASE_URL;
-    imageBaseURL = process.env.IMAGE_BASE_URL;
-  } catch (error) {
-    console.log(error);
-  }
-
-  console.log(`\
-Environment variables:
-PUBLIC_URL: ${process.env.PUBLIC_URL}
-API_BASE_URL: ${process.env.API_BASE_URL}
-IMAGE_BASE_URL: ${process.env.IMAGE_BASE_URL}\
-`
-  );
-
-  // Set reasonable defaults
-  if (publicURL == null) { publicURL = "/"; }
-  if (baseURL == null) { baseURL = "https://dev.macrostrat.org/image-tagger-api"; }
-  if (imageBaseURL == null) { imageBaseURL = "https://dev.macrostrat.org/image-tagger/img/"; }
-
-  console.log(publicURL, baseURL, imageBaseURL);
-
+  const env = getEnvironmentConfig(opts)
   // Image base url is properly set here
   const el = document.createElement('div');
   document.body.appendChild(el);
-  const __ = h(AppHolder, {baseURL, imageBaseURL, publicURL});
+  const __ = h(AppHolder, {...env});
   return render(__, el);
 };
 
