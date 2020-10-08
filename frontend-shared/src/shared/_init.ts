dc9f479f2ab4a394ea8e2093ea2e7101b4891b60
Daven Quinn <dev@davenquinn.com>
2020-10-08 15:34:22 -0500

    Removed bundled ui-components submodule

--- frontend-shared/src/shared/_init.ts
+++ frontend-shared/src/shared/_init.ts
@@ -4,7 +4,7 @@ import { FocusStyleManager } from "@blueprintjs/core";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/select/lib/css/blueprint-select.css";
-import "@macrostrat/ui-components/lib/index.css";
+import "@macrostrat/ui-components/lib/esm/index.css";
 
 FocusStyleManager.onlyShowFocusOnTabs();
 import "./main.styl";
