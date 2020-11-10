f8647326d55bd4e326e59fb178a89e1a2d053aab
Daven Quinn <dev@davenquinn.com>
2020-11-10 05:51:15 -0600

    Basic meta-visualizer app

---
+++ frontend-shared/src/xdd-app/index.ts
@@ -0,0 +1,84 @@
+import "~/shared/_init";
+import {
+  useAPIResult,
+  useAPIHelpers,
+  LinkButton,
+} from "@macrostrat/ui-components";
+import { render } from "react-dom";
+import h from "react-hyperscript";
+import { Route, useParams } from "react-router-dom";
+import { APIProvider } from "~/api";
+import { getEnvironmentConfig } from "~/shared/_env";
+import { KnowledgeBaseFilterView } from "~/visualizer-app/knowledge-base";
+import { AppRouter } from "~/shared/router";
+import { ButtonGroup, Button } from "@blueprintjs/core";
+import "./main.styl";
+
+const Visualizer = (props) => {
+  const { publicURL } = props;
+  return h(AppRouter, { basename: publicURL }, [
+    h(Route, {
+      path: "/",
+      exact: true,
+      component: KnowledgeBaseFilterView,
+    }),
+  ]);
+};
+
+function SetsSelector() {
+  const sets = useAPIResult("/sets");
+  if (sets == null) return null;
+  let { available_sets } = sets;
+  // Isn't yet in the lexicon
+  available_sets.push("geothermal");
+  return h("div.sets-selector", [
+    h("h1", "xDD sets"),
+    h(
+      ButtonGroup,
+      { large: true, vertical: true },
+      available_sets.map((d) => {
+        return h(LinkButton, { to: `/sets/${d}` }, d);
+      })
+    ),
+  ]);
+}
+
+function SetVisualizer() {
+  const { set } = useParams();
+  return h(
+    APIProvider,
+    { baseURL: `https://xdddev.chtc.io/sets/${set}/cosmos/api/v2_beta` },
+    h(KnowledgeBaseFilterView)
+  );
+}
+
+function App() {
+  return h(AppRouter, { basename: "/" }, [
+    h(Route, {
+      path: "/sets/:set",
+      component: SetVisualizer,
+    }),
+    h(Route, {
+      path: "/",
+      exact: true,
+      component: SetsSelector,
+    }),
+  ]);
+}
+
+const AppHolder = (props) => {
+  const { baseURL, imageBaseURL, publicURL, ...rest } = props;
+  return h(APIProvider, { baseURL: "https://xdddev.chtc.io" }, h(App));
+};
+
+const createUI = function (opts = {}) {
+  const env = getEnvironmentConfig(opts);
+  // Image base url is properly set here
+  const el = document.createElement("div");
+  document.body.appendChild(el);
+  const __ = h(AppHolder, { ...env });
+  return render(__, el);
+};
+
+// Actually run the UI (changed for webpack)
+createUI();
